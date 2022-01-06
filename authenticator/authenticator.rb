3ef05a58b9e6cf6725169a7edbedb3e83a135996
anepaz <anelisespaz@gmail.com>
2022-01-06 19:04:16 -0300

    Soft tabs updated

--- authenticator/authenticator.rb
+++ authenticator/authenticator.rb
@@ -8,7 +8,7 @@ users = [
 ]
 
 def auth_user(username, password, list_of_users)
-	list_of_users.each do |user_record|
+  list_of_users.each do |user_record|
 
 		if user_record[:username] == username && user_record[:password] == password
 			return user_record			
